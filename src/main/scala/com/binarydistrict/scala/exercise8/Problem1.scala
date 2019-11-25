0347f1d42f617ed216dfd43897717246bdaa7a1e
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-11-26 00:51:49 +0300

    some exercises were fixed

--- src/main/scala/com/binarydistrict/scala/exercise8/Problem1.scala
+++ src/main/scala/com/binarydistrict/scala/exercise8/Problem1.scala
@@ -7,7 +7,7 @@ object Problem1 {
     Реализовать API для своего Twitter-а
     POST /user                     json{username, session_id}                    - регистрация пользователя. session_id уникальный, по нему мы можем найти пользователя. Возвращает просто 200 или 409, если пользователи или session_id уже есть
     POST /post                     json{message, session_id}                     - отправить сообщение, возвращает message_id сообщения. В сообщении не должно быть больше 140 символов, иначе надо бросить код 400.
-    DELETE /post                   json{session_id, message_id}                  - удалить сообщение. 404, если нет
+    DELETE /post                   json{session_id, message_id}                  - удалить сообщение. 404, если нет. 403, если session_id не принадлежит пользователю
     GET /post/{username}           [json{message, username}]                     - получить сообщения пользователя. 404, если его нет
     GET /post/{username}/page/{n}  [json{message, username}]                     - получить сообщения пользователя по страницам по 3 сообщения на страницу в порядке отправления (старые сообщения идут раньше). 404, если его нет или страницы
     GET /stats                     [words_count, messages_count, users_count]    - получить статистику.
--- src/test/scala/com/binarydistrict/scala/exercise3/Exercise11Spec.scala
+++ src/test/scala/com/binarydistrict/scala/exercise3/Exercise11Spec.scala
@@ -19,20 +19,4 @@ class Exercise11Spec extends FlatSpec with Matchers {
     )
     buildMenu(menu) should contain theSameElementsInOrderAs Seq("A", "-G", "B", "-C", "--E", "---F", "-H", "D", "-I", "K")
   }
-  
-  "Solution" should "output the valid menu 2" in {
-    val menu = Seq(
-      MenuItem(1, "A", None),
-      MenuItem(2, "B", None),
-      MenuItem(3, "C", Some(2)),
-      MenuItem(4, "D", None),
-      MenuItem(5, "E", Some(3)),
-      MenuItem(6, "F", Some(5)),
-      MenuItem(7, "G", Some(1)),
-      MenuItem(8, "H", Some(2)),
-      MenuItem(9, "I", Some(10)),
-      MenuItem(10, "K", None),
-    )
-    buildMenu(menu) should contain theSameElementsInOrderAs Seq("A", "-G", "B", "-C", "--E", "---F", "-H", "D", "K", "-I")
-  }
 }
